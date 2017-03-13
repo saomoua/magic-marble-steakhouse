fbad4527a2bb0d7351dca6520497b0c63873e65f
Sae Vang <sae.vang@collegiseducation.com>
2017-03-12 23:05:55 -0500

    couldnt get form submission success function to work correctly. instead placed thank you message in alert box after form submission

--- src/js/forms.js
+++ src/js/forms.js
@@ -16,10 +16,7 @@ $("#form-contact").validate({
 
     submitHandler: function (form) {
         form.submit();
-    },
-    success: function () {
-        $('#form-contact').fadeOut();
-        $('#contact-success').fadeIn();
+        alert("Thank you for sending your message! We appreciate you taking the time to contact us, to let us know how we are doing and serving you. We hope that you share us with your friends and return to enjoy our food. If you have requested a response, we will get back to you within 48 business hours.") 
     }
 
 });
